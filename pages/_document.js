6da97e76fc7c8895860d83fa87179f0d2a6e4527
Hebert Zanatelli <hebert_alves1@hotmail.com>
2023-04-19 11:19:54 -0300

    estrutura inicial + material.ui

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,42 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import theme from '../src/theme'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta name='theme-color' content={theme.palette.primary.main} />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@material-ui/core"
+
+const theme = createTheme({
+    pallete: {
+        primary: {
+            main: '#000000'
+        }
+
+    }
+})
+
+export default theme
